7d00b59ca0a2ba1b3aff59b38415bbaa4bde53f7
anamariavv <anamaria.vargic45@gmail.com>
2022-04-11 18:28:15 +0200

    requestsender now receives application context

--- app/src/main/java/com/example/rmaprojekt/LoginActivity.java
+++ app/src/main/java/com/example/rmaprojekt/LoginActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -15,12 +16,11 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Context context = this;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        SingletonRequestSender.getInstance(this);
-
+        SingletonRequestSender.getInstance(getApplicationContext());
         //create login fragment
         Fragment loginFragment = new LoginFragment();
 
