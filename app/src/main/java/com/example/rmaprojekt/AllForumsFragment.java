c06fdf3e87ada500cabb8117330d156e7856b081
anamariavv <anamaria.vargic45@gmail.com>
2022-05-27 15:35:12 +0200

    display all forums added

--- app/src/main/java/com/example/rmaprojekt/AllForumsFragment.java
+++ app/src/main/java/com/example/rmaprojekt/AllForumsFragment.java
@@ -8,11 +8,27 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.VolleyError;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AllForumsFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
@@ -49,7 +65,102 @@ public class AllForumsFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        final float TITLE_TEXT_SIZE = 22;
+        final float TEXT_SIZE = 17;
+
         super.onViewCreated(view, savedInstanceState);
+
+        Map<String, String> forumInfo = new HashMap<>();
+        forumInfo.put("source", "display");
+
+        ScrollView forumScrollview = view.findViewById(R.id.all_forums_scrollview);
+        LinearLayout scrollChild = new LinearLayout(getContext());
+        scrollChild.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        scrollChild.setOrientation(LinearLayout.VERTICAL);
+        scrollChild.setShowDividers(LinearLayout.SHOW_DIVIDER_MIDDLE);
+        scrollChild.setDividerDrawable(getResources().getDrawable(R.drawable.empty_divider));
+
+        JSONObject requestBody = SingletonRequestSender.createJsonRequest(forumInfo);
+        SingletonRequestSender.sendRequest(requestBody, getResources().getString(R.string.forum_request_url), new SingletonRequestSender.RequestResult() {
+            @Override
+            public void onSuccess(JSONObject result) {
+
+                try {
+                    JSONArray allForums = result.getJSONArray("response");
+                    for(int i = 0; i < allForums.length(); i++) {
+                        JSONObject currentObject = allForums.getJSONObject(i);
+
+                        RelativeLayout forumLayout = new RelativeLayout(getContext());
+                        TextView title = new TextView(getContext());
+                        TextView text = new TextView(getContext());
+                        TextView author= new TextView(getContext());
+                        TextView id = new TextView(getContext());
+                        TextView published = new TextView(getContext());
+
+                        forumLayout.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                        forumLayout.setBackground(getResources().getDrawable(R.drawable.rounded_edittext));
+                        forumLayout.setPadding(10,20,0,20);
+                        forumLayout.setClickable(true);
+
+                        RelativeLayout.LayoutParams idParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                        id.setLayoutParams(idParams);
+                        id.setId(View.generateViewId());
+                        id.setText(currentObject.getString("id"));
+                        id.setVisibility(View.GONE);
+
+                        RelativeLayout.LayoutParams titleParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                        titleParams.addRule(RelativeLayout.BELOW, id.getId());
+                        title.setLayoutParams(titleParams);
+                        title.setId(View.generateViewId());
+                        title.setTextSize(TITLE_TEXT_SIZE);
+                        title.setText(currentObject.getString("title"));
+
+                        RelativeLayout.LayoutParams publishedParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                        publishedParams.addRule(RelativeLayout.BELOW, id.getId());
+                        publishedParams.addRule(RelativeLayout.END_OF, title.getId());
+                        publishedParams.addRule(RelativeLayout.ALIGN_BASELINE, title.getId());
+                        publishedParams.addRule(RelativeLayout.ALIGN_PARENT_END, RelativeLayout.TRUE);
+                        published.setLayoutParams(publishedParams);
+                        published.setId(View.generateViewId());
+                        published.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_END);
+                        published.setText(currentObject.getString("publish_date"));
+
+                        RelativeLayout.LayoutParams authorParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                        authorParams.addRule(RelativeLayout.BELOW, title.getId());
+                        author.setPadding(0,0,0,15);
+                        author.setLayoutParams(authorParams);
+                        author.setId(View.generateViewId());
+                        author.setText(currentObject.getString("author"));
+
+                        RelativeLayout.LayoutParams textParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                        textParams.addRule(RelativeLayout.BELOW, author.getId());
+                        text.setLayoutParams(textParams);
+                        text.setMaxLines(5);
+                        text.setTextSize(TEXT_SIZE);
+                        text.setEllipsize(TextUtils.TruncateAt.END);
+                        text.setId(View.generateViewId());
+                        text.setText(currentObject.getString("text"));
+
+                        forumLayout.addView(title);
+                        forumLayout.addView(author);
+                        forumLayout.addView(id);
+                        forumLayout.addView(published);
+                        forumLayout.addView(text);
+                        scrollChild.addView(forumLayout);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onError(VolleyError error) {
+                Toast toast = Toast.makeText(getContext(), "An error occurred", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+        forumScrollview.addView(scrollChild);
 
         FloatingActionButton fab = view.findViewById(R.id.forum_topics_action_button);
         fab.setOnClickListener(new View.OnClickListener() {
